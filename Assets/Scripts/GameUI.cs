5dd5a464a8aace4ec6935b1b632cf24538365fa4
chamutalz <chamutalz@gmail.com>
2020-09-07 17:04:15 +0300

    First commit

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour {
+
+	public GameObject gameOverUI;
+	public GameObject gameWinUI;
+	bool gameIsOver;
+
+	// Use this for initialization
+	void Start () {
+		Guard.OnGuardHasSpottedPlayer += ShowGameOverUI;
+		FindObjectOfType<Player>().OnReachedEndOfLevel += ShowGameWinUI;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameIsOver)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
+
+	void ShowGameWinUI()
+	{
+		OnGameOver(gameWinUI);
+	}
+
+	void ShowGameOverUI()
+	{
+		OnGameOver(gameOverUI);
+	}
+
+	void OnGameOver (GameObject showUI)
+	{
+		showUI.SetActive(true);
+		gameIsOver = true;
+		Guard.OnGuardHasSpottedPlayer -= ShowGameOverUI;
+		FindObjectOfType<Player>().OnReachedEndOfLevel -= ShowGameWinUI;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public event System.Action OnReachedEndOfLevel;
+
+	public float speed = 7;
+	public float smoothSpeed = .1f;
+	public float turnSpeed = 8;
+
+	float smoothedMagnitude;
+	float smoothVelocity;
+	float angle;
+	Vector3 velocity;
+
+	Rigidbody myRigidbody;
+	bool disabled;
+
+	private void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody>();
+		Guard.OnGuardHasSpottedPlayer += Disable;
+	}
+
+	private void Update()
+	{
+		Vector3 inputDirection = Vector3.zero;
+		if (!disabled)
+		{ inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized; }
+		float inputMagnitude = inputDirection.magnitude;
+		smoothedMagnitude = Mathf.SmoothDamp(inputMagnitude, smoothedMagnitude, ref smoothVelocity, smoothSpeed);
+		float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+		angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+		velocity = transform.forward * speed * smoothedMagnitude;
+		
+	}
+
+	private void FixedUpdate()
+	{
+		myRigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+		myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
+	}
+
+	private void Disable()
+	{
+		disabled = true;
+	}
+
+	private void OnDestroy()
+	{
+		Guard.OnGuardHasSpottedPlayer -= Disable;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Finish")
+		{
+			disabled = true;
+			if(OnReachedEndOfLevel != null)
+			{
+				OnReachedEndOfLevel();
+			}
+		}
+	}
+}
+
+
